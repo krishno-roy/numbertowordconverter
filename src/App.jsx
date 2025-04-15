935e93246aa1b4db6b03e54c4bec00bc63063d06
krishnoroy <krishnorooy@gmail.com>
2025-04-15 17:43:54 +0600

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ const App = () => {
     <div className="relative min-h-screen w-full bg-slate-950">
       {/* Background Glow Effects */}
       <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))] z-0"></div>
-      <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))] z-0"></div>
-
       {/* Conversion Section on Top */}
       <div className="relative z-10 pt-10 px-4">
         <Convertcase />
